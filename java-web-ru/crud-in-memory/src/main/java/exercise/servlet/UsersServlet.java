421da055000763c008f4d4156e83a848656c6967
@hexlet/cli <support@hexlet.io>
2022-12-22 19:16:02 +0300

    submit java-web-ru/crud-in-memory

---
+++ java-web-ru/crud-in-memory/src/main/java/exercise/servlet/UsersServlet.java
@@ -0,0 +1,250 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.RequestDispatcher;
+
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+
+import org.apache.commons.lang3.ArrayUtils;
+
+import static exercise.Data.getUsers;
+import static exercise.Data.getNextId;
+
+public class UsersServlet extends HttpServlet {
+
+    private List<Map<String, String>> users = getUsers();
+
+    private String getId(HttpServletRequest request) {
+        return request.getParameter("id");
+    }
+
+    private String getAction(HttpServletRequest request) {
+        String pathInfo = request.getPathInfo();
+        if (pathInfo == null) {
+            return "list";
+        }
+        String[] pathParts = pathInfo.split("/");
+        return ArrayUtils.get(pathParts, 1, "");
+    }
+
+    private Map<String, String> getUserById(String id) {
+        Map<String, String> user = users
+            .stream()
+            .filter(u -> u.get("id").equals(id))
+            .findAny()
+            .orElse(null);
+
+        return user;
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        String action = getAction(request);
+
+        switch (action) {
+            case "list":
+                showUsers(request, response);
+                break;
+            case "new":
+                newUser(request, response);
+                break;
+            case "edit":
+                editUser(request, response);
+                break;
+            case "show":
+                showUser(request, response);
+                break;
+            case "delete":
+                deleteUser(request, response);
+                break;
+            default:
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        String action = getAction(request);
+
+        switch (action) {
+            case "new":
+                createUser(request, response);
+                break;
+            case "edit":
+                updateUser(request, response);
+                break;
+            case "delete":
+                destroyUser(request, response);
+                break;
+            default:
+                response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void showUsers(HttpServletRequest request,
+                          HttpServletResponse response)
+                throws IOException, ServletException {
+
+        request.setAttribute("users", users);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/users.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+
+    private void showUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+        String id = getId(request);
+
+        Map<String, String> user = getUserById(id);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        request.setAttribute("user", user);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/show.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    private void newUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        // BEGIN
+        Map<String, String> user = new HashMap<>();
+        request.setAttribute("user", user);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/new.jsp");
+        requestDispatcher.forward(request, response);
+        // END
+    }
+
+    private void createUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        // BEGIN
+        Map<String, String> user = new HashMap<>();
+        user.put("id", getNextId());
+        user.put("firstName", request.getParameter("firstName"));
+        user.put("lastName", request.getParameter("lastName"));
+        user.put("email", request.getParameter("email"));
+        System.out.println(user.get("id"));
+        if (request.getParameter("firstName") == null || request.getParameter("lastName") == null) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/new.jsp");
+            request.setAttribute("user", user);
+            response.setStatus(422);
+            requestDispatcher.forward(request, response);
+            return;
+        }
+        users.add(user);
+        response.sendRedirect("/users");
+        // END
+    }
+
+    private void editUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        String id = getId(request);
+
+        Map<String, String> user = getUserById(id);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // BEGIN
+        request.setAttribute("user", user);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/edit.jsp");
+        requestDispatcher.forward(request, response);
+        // END
+    }
+
+    private void updateUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        String id = getId(request);
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        Map<String, String> user = getUserById(id);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        // BEGIN
+        Map<String, String> userUpdate = new HashMap<>();
+
+        userUpdate.put("id", id);
+        userUpdate.put("firstName", firstName);
+        userUpdate.put("lastName", lastName);
+        userUpdate.put("email", request.getParameter("email"));
+        System.out.println("hello!");
+
+        if ((firstName == null || firstName.isEmpty()) || (lastName == null || lastName.isEmpty())) {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/edit.jsp");
+            request.setAttribute("user", userUpdate);
+            response.setStatus(422);
+            request.setAttribute("error", "invalid input");
+            requestDispatcher.forward(request, response);
+            return;
+        }
+        users.remove(user);
+        users.add(userUpdate);
+        response.sendRedirect("/users");
+        // END
+    }
+
+    private void deleteUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        String id = getId(request);
+
+        Map<String, String> user = getUserById(id);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        request.setAttribute("user", user);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/delete.jsp");
+        requestDispatcher.forward(request, response);
+
+    }
+
+    private void destroyUser(HttpServletRequest request,
+                         HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        String id = getId(request);
+
+        Map<String, String> user = getUserById(id);
+
+        if (user == null) {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        users.remove(user);
+        response.sendRedirect("/users");
+    }
+}
---
+++ java-web-ru/crud-in-memory/src/main/webapp/edit.jsp
@@ -0,0 +1,34 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Edit user</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+            crossorigin="anonymous">
+    </head>
+    <body>
+        <div class="container">
+            <a href="/users">Все пользователи</a>
+            <!-- BEGIN -->
+            <div>${error}</div>
+            <form action="/users/edit" method="post">
+                <div class="mb-3">
+                    <input class="form-control" type="text" name="id" value=${user.get("id")} readonly>
+                    <label>Имя</label>
+                    <input class="form-control" type="text" name="firstName" value=${user.get("firstName")}>
+                    <label>Фамилия</label>
+                    <input class="form-control" type="text" name="lastName" value=${user.get("lastName")}>
+                    <label>Почта</label>
+                    <input class="form-control" type="text" name="email" value=${user.get("email")}>
+                </div>
+                <button type="submit" class="btn btn-primary">Изменить</button>
+            </form>
+            <!-- END -->
+        </div>
+    </body>
+</html>
