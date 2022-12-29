a5d267b8fa4dbaf5f4ceb35093512da6c8aaad5c
@hexlet/cli <support@hexlet.io>
2022-12-29 17:53:02 +0300

    submit java-web-ru/session

---
+++ java-web-ru/session/src/main/java/exercise/servlet/SessionServlet.java
@@ -0,0 +1,91 @@
+package exercise.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.RequestDispatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static exercise.App.getUsers;
+import exercise.Users;
+
+public class SessionServlet extends HttpServlet {
+
+    private Users users = getUsers();
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        if (request.getRequestURI().equals("/login")) {
+            showLoginPage(request, response);
+            return;
+        }
+
+        response.sendError(HttpServletResponse.SC_NOT_FOUND);
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request,
+                      HttpServletResponse response)
+                throws IOException, ServletException {
+
+        switch (request.getRequestURI()) {
+            case "/login" -> login(request, response);
+            case "/logout" -> logout(request, response);
+            default -> response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        }
+    }
+
+    private void showLoginPage(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+        requestDispatcher.forward(request, response);
+    }
+
+    private void login(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException, ServletException {
+
+        // BEGIN
+        HttpSession session = request.getSession();
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+        Map<String, String> user = users.findByEmail(email);
+
+        if (user != null && user.get("password").equals(password)) {
+            session.setAttribute("user", user);
+            session.setAttribute("flash", "Вы успешно вошли");
+            response.sendRedirect("/");
+        } else {
+            RequestDispatcher requestDispatcher = request.getRequestDispatcher("/login.jsp");
+            user = new HashMap<>();
+            user.put("email", email);
+            request.setAttribute("user", user);
+            session.setAttribute("flash", "unknown login or password");
+            response.setStatus(422);
+            requestDispatcher.forward(request, response);
+        }
+        // END
+    }
+
+    private void logout(HttpServletRequest request,
+                               HttpServletResponse response)
+                 throws IOException {
+
+        // BEGIN
+         HttpSession session = request.getSession();
+         // Удаляем атрибут из сессии
+         session.removeAttribute("userId");
+         response.sendRedirect("/");
+        // END
+    }
+}
