309ca558e429597d213155f62b6de5ac05cc66af
@hexlet/cli <support@hexlet.io>
2023-10-05 18:22:13 +0300

    submit java-spring-ru/authentication

---
+++ java-spring-ru/authentication/src/main/java/exercise/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package exercise;
+
+import exercise.model.User;
+import exercise.repository.UserRepository;
+
+import org.aspectj.apache.bcel.classfile.Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        // Список полномочий, которые будут предоставлены пользователю после аутентификации
+        List<SimpleGrantedAuthority> authorities = List.of(new SimpleGrantedAuthority("user"));
+
+        // BEGIN
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), authorities);
+        // END
+    }
+}
---
+++ java-spring-ru/authentication/src/main/java/exercise/controller/UserController.java
@@ -0,0 +1,37 @@
+package exercise.controller;
+
+import exercise.dto.UserDto;
+import exercise.model.User;
+import exercise.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    // Кодировщик BCrypt
+    // Используйте для хеширования пароля
+    private final PasswordEncoder encoder;
+
+    @GetMapping(path = "")
+    public Iterable<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    // BEGIN
+    @PostMapping(path = "")
+    public User createUser(@RequestBody UserDto userDto) {
+        User user = new User();
+        user.setUsername(userDto.username());
+        user.setEmail(userDto.email());
+        user.setPassword(encoder.encode(userDto.password()));
+        return userRepository.save(user);
+    }
+    // END
+}
